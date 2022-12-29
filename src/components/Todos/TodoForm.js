a7f1f62f556d1c9314ae18faaf2b339eb0d2784d
Roman <kolomrs@yandex.ru>
2022-12-30 00:11:09 +0300

    validate

--- src/components/Todos/TodoForm.js
+++ src/components/Todos/TodoForm.js
@@ -1,5 +1,6 @@
-import { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import Button from '../Ui/Button';
+import { validator } from '../Ui/validator';
 // import styles from './TodoForm.module.css';
 
 const initialState = {
@@ -8,11 +9,41 @@ const initialState = {
   textTodo: '',
 };
 
-function TodoForm({ addTodo }) {
+function TodoForm({ addTodo, completedTodosExist }) {
   const [formState, setFormState] = useState(initialState);
+  const [errors, setErrors] = useState({});
+
+  useEffect(() => {
+    validate();
+  }, [formState]);
+  const validatorConfig = {
+    email: {
+      isRequired: {
+        message: 'Электронная почта обязательна для заполнения',
+      },
+    },
+    username: {
+      isRequired: {
+        message: 'Имя пользователя обязателено для заполнения',
+      },
+    },
+    textTodo: {
+      isRequired: {
+        message: 'Текст задачи обязателен для заполнения',
+      },
+    },
+  };
+
+  const validate = () => {
+    const errors = validator(formState, validatorConfig);
+    setErrors(errors);
+    return Object.keys(errors).length === 0;
+  };
 
   const onSubmitHandler = (event) => {
     event.preventDefault();
+    const isValid = validate();
+    if (!isValid) return;
     addTodo(formState);
     setFormState(initialState);
   };
@@ -29,6 +60,7 @@ function TodoForm({ addTodo }) {
     <div className="form-control mb-3 bg-light">
       <form onSubmit={onSubmitHandler}>
         <label htmlFor="username">Имя пользователя</label>
+        <p style={{ color: 'red' }}>{errors && errors.username}</p>
         <input
           name="username"
           className="form-control mb-3"
@@ -36,7 +68,9 @@ function TodoForm({ addTodo }) {
           value={formState.username}
           onChange={onChangehandler}
         />
+
         <label htmlFor="email">Email</label>
+        <p style={{ color: 'red' }}>{errors && errors.email}</p>
         <input
           name="email"
           className="form-control mb-3"
@@ -45,6 +79,7 @@ function TodoForm({ addTodo }) {
           onChange={onChangehandler}
         />
         <label htmlFor="textTodo">Текст задачи</label>
+        <p style={{ color: 'red' }}>{errors.textTodo}</p>
         <input
           name="textTodo"
           className="form-control mb-3"
